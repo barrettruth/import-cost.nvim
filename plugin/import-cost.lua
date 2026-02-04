4fe850fc126a32a273c617ddf89df718764351af
Barrett Ruth <br.barrettruth@gmail.com>
2026-02-03 22:12:44 -0500

    fix: read custom filetypes from user config

--- plugin/import-cost.lua
+++ plugin/import-cost.lua
@@ -3,14 +3,17 @@ if vim.g.loaded_import_cost then
 end
 vim.g.loaded_import_cost = 1
 
+local user_config = vim.g.import_cost or {}
+local filetypes = user_config.filetypes or {
+  'javascript',
+  'javascriptreact',
+  'typescript',
+  'typescriptreact',
+  'svelte',
+}
+
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = {
-    'javascript',
-    'javascriptreact',
-    'typescript',
-    'typescriptreact',
-    'svelte',
-  },
+  pattern = filetypes,
   callback = function(args)
     require('import-cost').attach(args.buf)
   end,
